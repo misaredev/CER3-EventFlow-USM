9ccf081554efd47e5c4b96760bcb52ba389099ad
Desi <141796712+DesiList753@users.noreply.github.com>
2023-11-24 20:53:01 -0300

    agrega url de documentacion de la api

--- EventFlow/urls.py
+++ EventFlow/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework.documentation import include_docs_urls  #importa documentacion automatica de api
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    #path('docs/', include_docs_urls(title='EventFlow API')),
+    path('doc/', include_docs_urls(title='Documentacion de API')), #mas que nada es para tenerlo bonito
 ]
