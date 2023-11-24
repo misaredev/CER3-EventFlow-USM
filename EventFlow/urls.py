bf9f120ba43fc0a6505c0a359ee6a74176a1015d
Desi <141796712+DesiList753@users.noreply.github.com>
2023-11-24 18:56:37 -0300

    avance de modelos y coneccion via web

--- EventFlow/urls.py
+++ EventFlow/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #path('docs/', include_docs_urls(title='EventFlow API')),
 ]
--- UsmCalendar/models.py
+++ UsmCalendar/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+tipo = (
+    ('V', 'Vacaciones'),
+    ('F', 'Feriado'),
+    ('S', 'Suspension de actividades'),
+    ('SPM', 'Suspension de actividades PM'),
+    ('P', 'Periodo Lectivo'),
+    ('SE', 'Suspension de evaluaciones'),
+    ('C', 'Ceremonia'),
+    ('E', 'EDDA'),
+    ('EV', 'Evaluación'),
+    ('A', 'Ayudantías'),
+    ('H', 'Hito académico'),
+    ('SA', 'Secretaria Académica'),
+    ('O', 'OAI'),
+)
+segmento = (('1','comunidad USM'),('2', 'Estudiantes'),('3', 'Profesor'),('4', 'jefe de carrera '),)
+
+class evento(models.Model):
+    fecha_inicio = models.DateField()
+    fecha_termino = models.DateField()
+    titulo = models.CharField(max_length = 100)
+    descripcion = models.CharField(max_length=500)
+    tipo = models.CharField(max_length = 20,choices = tipo, default="V")
+    segmentos = models.ManyToManyField('segmento')
+
+    def __str__(self):
+        return self.titulo
+
+class Segmento(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nombre
