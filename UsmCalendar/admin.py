aad25f0d6d7f08ae1f881975677bd548686ebc93
Desi <141796712+DesiList753@users.noreply.github.com>
2023-11-25 18:27:30 -0300

    nose pero el models ahora tiene segmentos

--- UsmCalendar/admin.py
+++ UsmCalendar/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Evento
+
+from .models import Evento, Segmento
+
 # Register your models here.
 
 admin.site.register(Evento)
+admin.site.register(Segmento)
