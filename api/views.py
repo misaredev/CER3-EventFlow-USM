d8bd9df7cd4e0c80e4fb15324741749607fb124f
Desi <141796712+DesiList753@users.noreply.github.com>
2023-11-26 19:41:34 -0300

    url de api se utiliza api/evento/?dato=datobuscado o api/calendario/?dato=datobuscado

--- api/views.py
+++ api/views.py
@@ -5,5 +5,19 @@ from UsmCalendar.models import Evento
 # Create your views here.
 
 class EventoViewSet(viewsets.ModelViewSet):
-    queryset = Evento.objects.all()
     serializer_class = EventoSerializer
+    def get_queryset(self):
+        queryset = Evento.objects.all() #obtiene todo
+        fecha_inicio = self.request.query_params.get('fecha_inicio', None)
+        tipo = self.request.query_params.get('tipo', None)
+        segmento = self.request.query_params.get('segmento', None)
+        
+        #filtro por año, tipo y segmento
+        if fecha_inicio is not None:
+            queryset = queryset.filter(año=fecha_inicio)
+        if tipo is not None:
+            queryset = queryset.filter(tipo=tipo)
+        if segmento is not None:
+            queryset = queryset.filter(segmento=segmento)
+        
+        return queryset
