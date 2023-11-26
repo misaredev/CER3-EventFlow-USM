e3e93f5c85aed69964c19a6924caef93be43f3a5
Misael Tapia <misaeltapiac71@gmail.com>
2023-11-26 20:42:18 -0300

    test proximos eventos por segmento

--- UsmCalendar/views.py
+++ UsmCalendar/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from .models import Evento
+from .models import Evento, Segmento
 
 # Create your views here.
 
 def index(request):
     title = "EventFlow"
     Eventos = Evento.objects.all().order_by('fecha_inicio')
+    Segmentos = Segmento.objects.all()
 
     segmento = request.GET.get('selectSegmento')
     tipo = request.GET.get('selectTipo')
@@ -18,6 +19,7 @@ def index(request):
     data = {
         "title": title,
         "Eventos": Eventos,
+        "Segmentos": Segmentos,
     }
 
     return render(request, 'UsmCalendar/index.html', data)
